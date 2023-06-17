f9275afd98bbad58dd4957750edb16399af80bb2
himanshu54856 <122692417+himanshu54856@users.noreply.github.com>
2023-06-17 18:35:34 +0530

    Add files via upload

---
+++ week 6.1.py
@@ -0,0 +1,21 @@
+# Database is the collection of information or data in computer system .like: SQL,Oracle etc.
+'''SQL databases are relational and NoSQL database are non-relational.SQL database use structured query language (SQL) and have a predefined svchema.
+NoSQL databases have dynamic schemas for unstructured data.SQL databases are vertially scalable while NoSQL database are horizontal scalable.'''
+# DDL (Data Definition Language) their command as follow - CREATE,ALTER,DROP,TRUNCATE.
+''' CREATE -used to create a new table in SQL.EXAMPLE-
+CREATE TABLE Student_info(
+college_Id number(2),
+colege_name varchar(30)
+Branch varchar(10)
+);
+   ALTER- used to delete ,add or change column in the existing table.
+ALTER TABLE table_name;
+ADD column_name datatype:
+   TRUNCATE-used to remove all rows from table ,
+TRUNCATE TABLE table_name;
+    DROP-used to remove existing table along with its structure from the Database.
+DROP TABLE table_name:'''
+#DML-Data Manipulation Language.it cab be used to change the database.
+# INSERT-
+INSERT  INTO STUDENTS(Name,Department Id,DateOfBirth)
+INSERT INTO Students VALUES(cly,4,2 march22)
